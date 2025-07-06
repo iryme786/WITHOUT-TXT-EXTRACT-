684e57d6a54595ff3d8455964acdef32fa0b7223
Chiru <hasenorc@gmail.com>
2025-07-06 10:56:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "26468828"
+api_hash = "4693513c08d1ac6af15f95b116c29478"
+bot_token = "7933135334:AAHCB_cxEQ26KUoZHdXh61LPaG3SB-02ack"
+auth_users = [7517045929]
